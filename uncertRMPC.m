4151ca115353f071c62021e2e8ee1eace33ffd39
Lu-Shuwen <35024363+Lu-Shuwen@users.noreply.github.com>
2019-03-22 23:56:08 -0400

    tab

--- uncertRMPC.m
+++ uncertRMPC.m
@@ -10,42 +10,41 @@ function [gammaMin,gammaMax] = uncertRMPC(N,H)
 
 load('disturbancedata.mat','ww');
 
+% Hyper-rectangular uncertainty boundary of Wt(D) in equ (47)
+W = zeros(H,N);         
+for i = 1:N
+    W(:,i) = ww((i-1)*H+1:(i-1)*H+H)';
+                        % N columns meaning no. of scenarios
+                        % H rows meaning each scenario has H horizons
+end
 
-    % Hyper-rectangular uncertainty boundary of Wt(D) in equ (47)
-    W = zeros(H,N);         
-    for i = 1:N
-        W(:,i) = ww((i-1)*H+1:(i-1)*H+H)';
-                            % N columns meaning no. of scenarios
-                            % H rows meaning each scenario has H horizons
-    end
-    
-    % Solve for Hyper-rectangle captured scenarios based on equ (47)
-    gammaMin = sdpvar(H,1);
-    gammaMax = sdpvar(H,1);
-    
-    % Define constraints 
-    Cons = [];
-    for i = 1:N
-        Cons = [Cons, gammaMin <= W(:,i), gammaMax >= W(:,i)];
-    end
-    
-    % Define an objective
-    Obj = norm(gammaMax-gammaMin,1);
-    
-    % Set some options for YALMIP and solver
+% Solve for Hyper-rectangle captured scenarios based on equ (47)
+gammaMin = sdpvar(H,1);
+gammaMax = sdpvar(H,1);
+
+% Define constraints 
+Cons = [];
+for i = 1:N
+    Cons = [Cons, gammaMin <= W(:,i), gammaMax >= W(:,i)];
+end
+
+% Define an objective
+Obj = norm(gammaMax-gammaMin,1);
+
+% Set some options for YALMIP and solver
 %     opt = ...
 %         sdpsettings('verbose',1,'solver','quadprog','quadprog.maxiter',100);
 %     
-    % Solve the problem
-    sol = optimize(Cons,Obj,'');
-    
-    % Analyze error flags
-    if sol.problem == 0
-     % Extract value
-        gammaMin = value(gammaMin);
-        gammaMax = value(gammaMax);
-    else
-        disp('One more step close to success');
-        sol.info
-        yalmiperror(sol.problem)
-    end 
+% Solve the problem
+sol = optimize(Cons,Obj,'');
+
+% Analyze error flags
+if sol.problem == 0
+% Extract value
+    gammaMin = value(gammaMin);
+    gammaMax = value(gammaMax);
+else
+    disp('One more step close to success');
+    sol.info
+    yalmiperror(sol.problem)
+end 
